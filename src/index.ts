828f5e7086f3e035ec15621f92a838ee379838e1
dtakkiy <dtakkiy00@gmail.com>
2026-02-22 03:20:01 +0900

    ロガー導入 (#10)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ dotenv.config();
 import { serve } from '@hono/node-server';
 import app from './app';
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled rejection:', reason);
+  process.exit(1);
+});
+
 const PORT = Number(process.env.PORT ?? 3000);
 
 serve({ fetch: app.fetch, port: PORT }, () => {
